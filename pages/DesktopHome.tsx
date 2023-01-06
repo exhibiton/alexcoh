124f2c7e396cda6bff676626fa6fc468ac7bc76b
Jesse <jessevheikkila@gmail.com>
2023-01-06 14:56:49 +0200

    shit version of services

--- pages/DesktopHome.tsx
+++ pages/DesktopHome.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import NavigationBar from '../components/NavigationBar'
 import styles from './page.module.css'
 import cyclist from '../images/mainCyclist.png'
+import cohIcon from '../public/cohIcon.svg'
 
 export default function Home() {
   return (
@@ -14,6 +15,22 @@ export default function Home() {
           </div>
           <div className={styles.heroText}>
             Triathlon, cycling and run coaching by <u>Alex Coh</u>
+          </div>
+        </div>
+        <div className={styles.contactButton}>Contact Now</div>
+        <div className={styles.services}>
+          <div className={styles.servicesSection}>
+            <div className={styles.servicesText}>
+              <div className={styles.servicesTitle}>
+                We offer coaching services for athletes internationally around the world
+              </div>
+              <div className={styles.servicesSubtitle}>
+                Time to start your health and fitness journey today - discover how!
+              </div>
+            </div>
+            <div className={styles.cohIcon}>
+              <Image src={cohIcon} alt="Coh" />
+            </div>
           </div>
         </div>
       </div>
