b032b3e0905e2cc818150d7728b03b2362d8e8ce
Jesse <jessevheikkila@gmail.com>
2023-01-06 19:49:04 +0200

    add nutrtion content

--- pages/services/nutrition/index.tsx
+++ pages/services/nutrition/index.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image'
 import { TabLink } from '../../../components/TabLink'
+import Link from 'next/link'
 import NavigationBar from '../../../components/NavigationBar'
-import styles from '../ServicesDesktop.module.css'
+import styles from './ServicesNutrition.module.css'
+import nutrition1 from '../../../images/nutrition1.jpg'
+import Footer from '../../../components/Footer'
 
 export default function ServicesNutrition() {
   return (
@@ -19,12 +23,29 @@ export default function ServicesNutrition() {
           </div>
         </div>
       </div>
+      <div className={styles.coachingOne}>
+        <Image src={nutrition1} alt="Nutrition Services" />
+      </div>
+
       <div className={styles.mainContainer}>
         <div className={styles.mainTitle}>
-          Alex Coh&apos;s nutrition planning is <br />
+          Alex Coh's nutrition planning is <br />
           custom made for every client.
         </div>
       </div>
+      <div className={styles.mainBody}>
+        <div className={styles.mainBodyText}>
+          I consider your body type, food allergies, your sport of choice, training volume and intensity
+          (where applicable), and create a plan that will provide your body with the right nutrients for
+          optimal health and performance. <br /> <br />
+          Nutrition plans also include fueling for training and events as needed (not limited to endurance
+          sports).
+        </div>
+        <div className={styles.getInTouch}>
+          <Link href={'/contact'}>Get in touch</Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
