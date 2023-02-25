53700eab321dc590a6d3b7715651aee75fd08d54
Jesse <jessevheikkila@gmail.com>
2023-02-25 17:31:57 +0200

    services nutrition and coaching

--- pages/services/coaching/index.tsx
+++ pages/services/coaching/index.tsx
@@ -5,6 +5,7 @@ import NavigationBar from '../../../components/NavigationBar'
 import styles from '../ServicesDesktop.module.css'
 import coaching1 from '../../../images/coaching1.jpg'
 import Footer from '../../../components/Footer'
+import services1 from '../../../public/services1.svg'
 
 export default function ServicesCoaching() {
   return (
@@ -41,6 +42,66 @@ export default function ServicesCoaching() {
           fiber type (determining what kind of training works best) and am focused on giving the best possible
           results within your available time for training. <br /> <br /> We all have jobs, family commitments,
           friends, and every minute of training needs to count.
+        </div>
+        <div className={styles.getInTouch}>
+          <Link href={'/contact'}>Get in touch</Link>
+        </div>
+      </div>
+      <div className={styles.carouselSection}>
+        <div className={styles.carousel}>
+          <div className={styles.carouselPicture}>
+            <Image src={services1} alt="Services 1" />
+          </div>
+          <div className={styles.carouselText}>
+            <div className={styles.carouselTextTitle}>Triathlon coaching</div>
+            <div className={styles.carouselTextContent}>
+              Includes a monthly plan with swimming, running and cycling workouts for indoor and outdoor use
+              and also includes a core and stretching regimen with as many changes to the plan as are needed
+              to make the training fit into your schedule.
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>200€</a> / Month
+            </div>
+          </div>
+          <div className={styles.carouselDarkText}>
+            <div className={styles.carouselTextTitle}>
+              Cycling <br />
+              coaching
+            </div>
+            <div className={styles.carouselTextContent}>
+              Includes a monthly plan for cycling with indoor and outdoor workouts and also includes a core
+              and stretching regimen with as many changes to the plan as are needed to make the training fit
+              into your schedule.
+              <br />
+              <br />
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>150€</a> / Setup
+            </div>
+          </div>
+          <div className={styles.carouselText}>
+            <div className={styles.carouselTextTitle}>
+              Run <br />
+              coaching
+            </div>
+            <div className={styles.carouselTextContent}>
+              Includes a monthly plan for running and also includes a core and stretching regimen with as many
+              changes to the plan as are needed to make the training fit into your schedule.
+              <br />
+              <br />
+              <br />
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>150€</a> / Setup
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.secondBody}>
+        <div className={styles.mainBodyText}>
+          Included in the price is daily feedback on the workouts, 2 calls/discussions per month, general
+          nutrition guidelines and fuelling for racing and training.
         </div>
         <div className={styles.getInTouch}>
           <Link href={'/contact'}>Get in touch</Link>
--- pages/services/nutrition/index.tsx
+++ pages/services/nutrition/index.tsx
@@ -5,6 +5,8 @@ import NavigationBar from '../../../components/NavigationBar'
 import styles from './ServicesNutrition.module.css'
 import nutrition1 from '../../../images/nutrition1.jpg'
 import Footer from '../../../components/Footer'
+import services1 from '../../../public/services1.svg'
+import services4 from '../../../public/services4.svg'
 
 export default function ServicesNutrition() {
   return (
@@ -40,6 +42,58 @@ export default function ServicesNutrition() {
           optimal health and performance. <br /> <br />
           Nutrition plans also include fueling for training and events as needed (not limited to endurance
           sports).
+        </div>
+        <div className={styles.getInTouch}>
+          <Link href={'/contact'}>Get in touch</Link>
+        </div>
+      </div>
+
+      <div className={styles.carouselSection}>
+        <div className={styles.carousel}>
+          <div className={styles.carouselPicture}>
+            <Image src={services1} alt="Services 1" />
+          </div>
+          <div className={styles.carouselText}>
+            <div className={styles.carouselTextTitle}>Non coached athletes</div>
+            <div className={styles.carouselTextContent}>
+              Includes a monthly plan with swimming, running and cycling workouts for indoor and outdoor use
+              and also includes a core and stretching regimen with as many changes to the plan as are needed
+              to make the training fit into your schedule.
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>300€</a> / Setup
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>100€</a> / Month
+            </div>
+          </div>
+          <div className={styles.carouselDarkText}>
+            <div className={styles.carouselTextTitle}>Coached athletes</div>
+            <div className={styles.carouselTextContent}>
+              Includes a monthly plan for running and also includes a core and stretching regimen with as many
+              changes to the plan as are needed to make the training fit into your schedule.
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>150€</a> / Setup
+            </div>
+            <div className={styles.carouselPrice}>
+              <a className={styles.carouselPriceBold}>20€</a> / Month
+            </div>
+          </div>
+          <div className={styles.carouselPicture}>
+            <Image src={services4} alt="Services 4" />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.secondBody}>
+        <div className={styles.mainBodyText}>
+          All nutrition plans include a long interview with you where we focus on your nutrition habit
+          history, food allergies and go through your overall feeling of energy levels, sleep quality,
+          recovery. <br /> <br />
+          Nutrition plans are then custom built for every customer based on that interview and include
+          complete recipes that are simple and efficient. The key to an efficient nutrition plan is to make it
+          user friendly – easy to execute and delicious.
         </div>
         <div className={styles.getInTouch}>
           <Link href={'/contact'}>Get in touch</Link>
