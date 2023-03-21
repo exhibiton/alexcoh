91d404a253f7596dda5874b32eb51ad77ca65bd2
Jesse <jessevheikkila@gmail.com>
2023-03-21 14:07:57 +0200

    first mobile part, can it really work like this?

---
+++ components/MobileNavigationBar.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import styles from './MobileNavigationBar.module.css'
+import cohIcon from '../public/alexCohLogo.svg'
+import navigationLink from '../public/navigationLink.svg'
+
+export default function Home() {
+  return (
+    <div>
+      <div className={styles.navigationContainer}>
+        <div className={styles.cohIcon}>
+          <Image src={cohIcon} alt="Coh" />
+        </div>
+        <div className={styles.navigationIcon}>
+          <Image src={navigationLink} alt="Coh" />
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/MobileHome.tsx
+++ pages/MobileHome.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image'
 import styles from './mobile.module.css'
-
+import MobileNavigation from '../components/MobileNavigationBar'
+import cyclist from '../images/mainCyclist.png'
+import Link from 'next/link'
 export default function Home() {
   return (
-    <div>
-      <div className={styles.normalTitle}>
-        <b>Alex Coh</b>
+    <div className={styles.container}>
+      <MobileNavigation />
+      <div className={styles.heroTitle}>
+        <div className={styles.heroImage}>
+          <Image src={cyclist} alt="Cyclist" placeholder="blur" />
+        </div>
+        <div className={styles.heroText}>
+          Triathlon, cycling and <br />
+          run coaching by <u>Alex Coh</u>
+        </div>
+        <Link href="/contact">
+          <div className={styles.contactNow}>Contact Now</div>
+        </Link>
       </div>
     </div>
   )
