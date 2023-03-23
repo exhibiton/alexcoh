98e488ac3968a19e92e72f434ce8a5c88cfd8f73
Jesse <jessevheikkila@gmail.com>
2023-03-23 14:41:15 +0200

    part of mobile

--- pages/MobileHome.tsx
+++ pages/MobileHome.tsx
@@ -3,21 +3,25 @@ import styles from './mobile.module.css'
 import MobileNavigation from '../components/MobileNavigationBar'
 import cyclist from '../images/mainCyclist.png'
 import Link from 'next/link'
+import cohMobile from '../public/cohmobile.svg'
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <MobileNavigation />
-      <div className={styles.heroTitle}>
-        <div className={styles.heroImage}>
-          <Image src={cyclist} alt="Cyclist" placeholder="blur" />
+    <div>
+      <div className={styles.container}>
+        <MobileNavigation />
+        <div className={styles.heroTitle}>
+          <div className={styles.heroImage}>
+            <Image src={cyclist} alt="Cyclist" placeholder="blur" />
+          </div>
+          <div className={styles.heroText}>
+            Triathlon, cycling and <br />
+            run coaching by <u>Alex Coh</u>
+          </div>
+          <Link href="/contact">
+            <div className={styles.contactNow}>Contact Now</div>
+          </Link>
         </div>
-        <div className={styles.heroText}>
-          Triathlon, cycling and <br />
-          run coaching by <u>Alex Coh</u>
-        </div>
-        <Link href="/contact">
-          <div className={styles.contactNow}>Contact Now</div>
-        </Link>
       </div>
     </div>
   )
