782974507f7ef25f1ce15632260c95784a9995eb
star-works <farukhkhan1115@gmail.com>
2023-03-25 23:56:27 +0530

    fixed screen on overlay

--- components/mobil/common/Navbar.tsx
+++ components/mobil/common/Navbar.tsx
@@ -1,10 +1,19 @@
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { LogoIcon, MenuIcon } from './Icon'
 import Sidebar from '../sidebar/Sidebar'
 
 function Navbar() {
   const [isSidebarOpen, setSidebarOpen] = useState(false)
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.body.classList.add('fixed-screen')
+    } else {
+      document.body.classList.remove('fixed-screen')
+    }
+  }, [isSidebarOpen])
+
   return (
     <>
       <div className="mob-nav  my-container">
