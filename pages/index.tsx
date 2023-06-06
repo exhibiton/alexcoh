16c866e3f5bb06d89946d6b2d3c4bded773d236b
Jesse <jessevheikkila@gmail.com>
2023-06-06 20:10:50 +0300

    Add PostHog and SEO changes for Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,19 @@ export default function Home() {
     <>
       <Head>
         <title>Alex Coh Triathlon, Cycling and Run Coaching</title>
+        <meta name="title" content="Alex Coh Triathlon, Cycling and Run Coaching" />
         <meta
           name="description"
           content="Alex Coh provides premium endurance coaching in Triathlon, Cycling and Running."
         />
+        <meta
+          name="keywords"
+          content="alex, alex coh, triathlon coaching, cycling coaching, running coaching, run coaching, coaching, endurance coaching"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
       </Head>
       <MediaContextProvider disableDynamicMediaQueries>
         <Media at="xs">
